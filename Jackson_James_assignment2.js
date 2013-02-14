f058dd33f93959b3a3830e68f76fc3efc7341720
James <jamesdjackson@gmail.com>
2013-02-14 18:42:53 -0500

    Final commit.
    Modified var modifier to story to be more acurrate in last array
    function and finished adding values to array.  Project complete.

--- Jackson_James_assignment2.js
+++ Jackson_James_assignment2.js
@@ -64,20 +64,63 @@ console.log(ghosts + "and " + monsters + " are found throughout this work.");
 //Array Function with Math
 
 var Master = [
+		"Samanosuke Mitsutoshi",
+		"Lord Sadanobu",
+		"Oda Harunaga",
+		"Kusunoki Tamon Masayuki",
+		"Kiyohime",
+		"Watanabe Genjitsuna",
+		"Tawara Toda Hidesato",
+		"Fuwa Bansaku",
+		"Sagiike Heikuro",
         "Oya Taro Mitsukuni",
-        "Onogawa Kisaburo",
-        "Shobu"
+        "Shirafuji Genta",
+    	"Onogawa Kisaburo",
+    	"Iga no Tsubone",
+    	"Toki Daishiro",
+    	"Minamoto Yorimitsu Ason",
+    	"Kayo Fujin",
+    	"Raishin",
+    	"Nyuunryu Kosonsho",
+    	"Shobu",
+    	"Donyoku no baba",
+    	"Nikki Dansei Naonori",
+    	"Shusuinosuke Tobe Suetake",
+    	"Miyamoto Musashi"
+    	
 ];
 
-var modifiers = [
+
+var stories = [
+		"a great retainer who's saved a close friend's daughter who was bewitched by a fox.",
+        "a powerful government figure whom was attacked by a demon in the emperor's southern palace.",
+        "a powerful warlord that requested the removal of a temple and was haunted by a remaining tree.",
+        "a little boy snuck out to play with a wooden horse only to be menaced by a ghost from within the toy.",
+        "a story of a woman so overcome by jealousy it transforms her into a serpent like creature.",
+        "a brilliant and courageous samurai that defeats a demon attacking passing travelers.",
+        "a brilliant archer that slays the giant centipede of Mount Mikami.",
+        "a member of the Mori samurai family that exposed the ghosts of a local temple.",
+        "a naturally courageous man who observes a giant serpent while fishing and dismembers it with his bare hands.", 
         "a retainer who saw a vision of hundreds of skeletons fighting.",
+        "a son of a farmer renowned for his sumo abilities who defeated an infamous kappa spirit and killed it.",
         "a great sumo wrestler who encounters a monster while standing guard in his master's castle.",
+        "a young courtesan summoned in the middle of the night by a mysterious ghost.",
+        "a young vassal intrigued by stories of ghosts at a near by temple that decides to confront them.",
+        "a retainer that lay ill in bed that was attacked by demons.",
+        "a beautiful woman that used her overpowered a kingdom and eventually caused a rebellion by disturbing Buddhist precepts.",
+        "a Chinese thunder god is sent to defeat an invading army.",
+        "a hero of the 108 Suikoden story, he was able to conger magic by making mudra signs leading to his name Dragon Entering Clouds.", 
         "a master archer that slays a serpent spirit responsible for slaying several military elephants.",
+        "a little old woman who is tortured by evil ghosts and suffers at the devices of her own greed.",
+        "a ruthless traitor that attempted to murder his infant master in his sleep.",
+        "a famous retainer who is said to have encountered the ghost of a pregnant woman late one evening.",
+        "a great sword fighter that defeated an acetic mountain man that turned into a tengu or heavenly dog after being killed.",
+        
 ];
 
 for (var i=0, j=Master.length; i < j; i++) {
 
-        console.log ( "The story of " + Master[i] + " is about " + modifiers[i] );
+        console.log ( "The story of " + Master[i] + " is about " + stories[i] );
 };
 
-console.log ("I hope you know a little more about Yoshitoshi's 36 Ghosts now!");
+console.log ("These are only the first 23, but I hope you know a little more about Yoshitoshi's 36 Ghosts now!");
