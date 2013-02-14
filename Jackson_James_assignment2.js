3646670015b079a111070295004aff8737c8c245
James <jamesdjackson@gmail.com>
2013-02-14 16:39:51 -0500

    Cocnatenation Function Added
    Last required function added.  Other code cleaned up.

--- Jackson_James_assignment2.js
+++ Jackson_James_assignment2.js
@@ -1,32 +1,29 @@
-//Procedure
+//James Jackson SDI 1302 Assignment 2
 
 //Argument Conditional using "if statement" to something
 
 //send output to console
 
-//Back- send control back to main code
 
-
-
-var SeriesLength = 35;
+var SeriesLength = 36;
 
 if(SeriesLength == 36){
 
 console.log ("We are talking about one of the 36");
 
-}else{	
+}else{
 
-console.log ("We are not talking about one of our series.");	
+console.log ("We are not talking about one of our series.");
 
 }
 
+ 
 
-
-//Comparison conditional using "if statement" sending two arguments 
+//Comparison conditional using "if statement" sending two arguments
 
 //Return a value from the declared function
 
-var SeriesLength = 37
+ 
 
 if(SeriesLength == 36){
 
@@ -46,22 +43,46 @@ console.log ("It is definitely not the 36 Views of Mt Fuji or 36 Ghosts.")
 
 }
 
+ 
 
-
-//Number Function with While Loop
+//Number Function with "While Loop"
 
 var viewsOfMtFuji = 36
 var prints = function(total) {
 var views = 0;
 while (views < viewsOfMtFuji) {
         console.log("Hokusai is said to have created " + views + " views of Mt Fuji, or maybe")
-		views+=4;
+                views+=4;
 };
 return viewsOfMtFuji;
 
 }
 console.log ( prints (" is the total number of views of Mt Fuji by Hokusai."));
 
+ 
 
+//String Function with two string concatenation
 
+var ghosts = "Ghosts "; 
+var monsters = "monsters ";
 
+console.log(ghosts + " and " + monsters + " are found throughout his work.");
+
+//Array Function with Math
+
+var Master = [
+        "Hokusai",
+        "Yoshitoshi",
+        "Kuniyoshi"
+];
+
+var modifiers = [
+        "Oldest",
+        "Craziest",
+        "Youngest",
+];
+
+for (var i=0, j=Master.length; i < j; i++) {
+
+        console.log ( "Master " + Master[i] + " was " + modifiers[i] );
+};
